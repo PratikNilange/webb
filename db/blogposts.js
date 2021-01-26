5240fd8dcd21c41a44900622248a3e345d2a4a40
PratikNilange <PratikNilange@users.noreply.github.com>
2021-01-26 13:59:49 +0530

    maj ed;

---
+++ db/blogposts.js
@@ -0,0 +1,17 @@
+var $table = $('#table');
+var mydata = 
+      [ 
+        { 
+          "article": "<a href=\"posts/2020/Checking-out-new-theme-Seedlet.html\" class=\"normallink text-wrap\">Checking out new theme – Seedlet</a>", 
+          "date": "August 31, 2020", 
+        //   "job": "Carpenter",
+        //   "city": "New York"
+        }, 
+        { 
+          "article": "<a href=\"#\" class=\"normallink text-wrap\">Goodbye, Google Plus</a></span>", 
+          "date": "June 7, 2020", 
+        //   "job": "Engineer",
+        //   "city": "New York"
+        },
+        
+      ]; 
--- license.md
+++ license.md
@@ -5,3 +5,5 @@
 - **jQuery**: Type/JS: Copyright OpenJS Foundation and other contributors: The MIT License (MIT)
 - **Bootstrap**: Type/CSS-JS: Copyright (c) 2011-2021 Twitter, Inc. Copyright (c) 2011-2021 The Bootstrap Authors: The MIT License (MIT)
 - **popperjs**: Type/JS: Copyright (c) 2019 Federico Zivolo: The MIT License (MIT)
+- **Source Serif Pro**: Type/Font: Principal Designer-Frank Grießhammer: Licensed under the Open Font License.
+- **Bootstrap-tables**: Type/CSS-JS: Zhixin Wen: The MIT License
